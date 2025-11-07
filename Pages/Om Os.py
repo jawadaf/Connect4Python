23cb0ef0f4f7c7efae6a730957fe631c94e29196
jawadaf <jafa0001@stud.kea.dk>
2025-11-07 16:30:25 +0100

    little refracotr

--- Pages/Om Os.py
+++ Pages/Om Os.py
@@ -1,13 +1,9 @@
 import streamlit as st
 
 def om_os():
-    st.title("Om os")
-    st.write("Vi er en gruppe passionerede udviklere, som har skabt et interaktivt Connect4-spil.")
-    st.write("**Teamet bag projektet:**")
-    st.write("- Hamse Hassan - Full-Stack Developer")
-    st.write("- Ali Selimi - Frontend Specialist")
-    st.write("- Jawad Ali Fakhreddin - UI/UX Designer")
-    st.write("\nVi arbejder sammen om at udvikle sjove og engagerende spiloplevelser.")
+    st.title("Om mig")
+    st.write("Jawad Ali Fakhreddin...")
+    st.write("linkedin: https://www.linkedin.com/in/jawad-ali-fakhreddine/")
 
 if __name__ == "__main__":
     om_os()
